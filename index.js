7ceee4c44d0ef83c5fbae2ee4975c58a97bd81d8
Brycen Dorsay <bdorsay@baileynelson.com>
2019-12-24 16:49:51 -0500

    message.json file

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-const message = {
-  blocks: [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: 'This is a plain text section block.',
-      },
-    },
-    {
-      type: 'divider',
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text:
-          'This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>',
-      },
-    },
-  ],
-};
+const axios = require('axios');
 
 async function main() {
+  const message = github.core.getInput('message');
   const response = await axios.post(process.env.SLACK_WEBHOOK, message);
   console.log(response);
 }
