045bf519d213a1282fde368b649d885357a2e279
Brycen Dorsay <bdorsay@baileynelson.com>
2019-12-24 17:07:41 -0500

    don't need @actions/github

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const axios = require('axios');
 
 async function main() {
-  const message = github.core.getInput('message');
-  const response = await axios.post(process.env.SLACK_WEBHOOK, 'message');
+  const message = core.getInput('message');
+  const response = await axios.post(process.env.SLACK_WEBHOOK, message);
   console.log(response);
 }
 
