46b5de9a82a637917d9bb158b7b1f6781bd0b0b5
Brycen Dorsay <bdorsay@baileynelson.com>
2019-12-24 17:30:04 -0500

    working without logs

--- index.js
+++ index.js
@@ -4,17 +4,9 @@ const fs = require('fs');
 
 async function main() {
   const messageDir = core.getInput('message');
-  fs.readdir('./', function(err, items) {
-    console.log(items);
-
-    for (var i = 0; i < items.length; i++) {
-      console.log(items[i]);
-    }
-  });
   const message = fs.readFileSync(messageDir);
-  console.log(message);
   const response = await axios.post(process.env.SLACK_WEBHOOK, message);
-  console.log(response);
+  // console.log(response);
 }
 
 try {
