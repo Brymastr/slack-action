2bb971962f2a3279c1ccf500bb9f7846c3fa5614
Brycen Dorsay <bdorsay@baileynelson.com>
2019-12-24 17:24:28 -0500

    readdir

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 
 async function main() {
   const messageDir = core.getInput('message');
-  console.log(fs.readDirSync('./'));
+  fs.readdir('./', function(err, items) {
+    console.log(items);
+
+    for (var i = 0; i < items.length; i++) {
+      console.log(items[i]);
+    }
+  });
   const message = fs.readFileSync(messageDir);
   console.log(message);
   const response = await axios.post(process.env.SLACK_WEBHOOK, message);
