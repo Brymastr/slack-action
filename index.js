7a1f9fce00b724a0adca11e9d1a85aca1777cadd
Brycen Dorsay <bdorsay@baileynelson.com>
2019-12-24 22:40:01 -0500

    vars

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const core = require('@actions/core');
 const axios = require('axios');
 const fs = require('fs');
 
-console.log(Object.entries(process.env));
+const commit = process.env['GITHUB_SHA'];
+const repo = process.env['GITHUB_REPOSITORY'];
+const ref = process.env['GITHUB_REF'];
+const workflow = process.env['GITHUB_WORKFLOW'];
+const event = process.env['GITHUB_EVENT_NAME'];
+const url = `https://github.com/${repo}/commit/${commit}/checks`;
+
+const variables = Object.entries(process.env).filter(x => x[0].startsWith('SLACK_'));
+
+console.log(url);
+console.log(variables);
 // async function main() {
 //   const messageDir = core.getInput('message');
 //   const message = fs.readFileSync(messageDir);
