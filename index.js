2c52da6d77445a40f3960d8c1bbe0d2a6fd3590c
Brycen Dorsay <bdorsay@baileynelson.com>
2019-12-24 22:07:40 -0500

    log env

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const core = require('@actions/core');
 const axios = require('axios');
 const fs = require('fs');
 
-async function main() {
-  const messageDir = core.getInput('message');
-  const message = fs.readFileSync(messageDir);
-  const response = await axios.post(process.env.SLACK_WEBHOOK, message);
-  // console.log(response);
-}
+console.log(Object.entries(process.env));
+// async function main() {
+//   const messageDir = core.getInput('message');
+//   const message = fs.readFileSync(messageDir);
+//   await axios.post(process.env.SLACK_WEBHOOK, message).catch(err => {
+//     throw new Error(err);
+//   });
+// }
 
-try {
-  main();
-} catch (err) {
-  core.setFailed(err.message);
-}
+// try {
+//   main();
+// } catch (err) {
+//   core.setFailed(err.message);
+// }
